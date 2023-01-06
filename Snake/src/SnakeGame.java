7cd2e604cb2d250073a9af9d04e7264dbb8fd676
Harshil1823 <112011387+Harshil1823@users.noreply.github.com>
2023-01-05 21:57:15 -0500

    Add files via upload

---
+++ Snake/src/SnakeGame.java
@@ -0,0 +1,14 @@
+
+public class SnakeGame {
+
+	public static void main(String[] args) {
+
+		//creating instance of gameGrame class
+		//writign new GameFrame(); can also create the instance. But you won't be able to use it. 
+		GameFrame frame = new GameFrame();
+		
+		
+		
+	}
+
+}
